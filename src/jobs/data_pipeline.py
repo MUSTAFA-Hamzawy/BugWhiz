e89744f89602087b1bde90d68f11f8ecddc488e6
Karim Mahmoud <82693464+karimmahmoud22@users.noreply.github.com>
2024-03-04 05:40:13 +0200

    prepare the evaluation: recall & retrival

--- src/jobs/data_pipeline.py
+++ src/jobs/data_pipeline.py
@@ -18,6 +18,7 @@ class DataPipeline:
     def __init__(self, DATASET, DOMAIN, 
                     PREPROCESSING, VALIDATION_SPLIT=0.9,
                     MAX_SEQUENCE_LENGTH_TITLE=50, MAX_SEQUENCE_LENGTH_DESC=150):
+        
         self.MAX_NB_WORDS = 20000
         self.MAX_SEQUENCE_LENGTH_T = MAX_SEQUENCE_LENGTH_TITLE
         self.MAX_SEQUENCE_LENGTH_D = MAX_SEQUENCE_LENGTH_DESC
@@ -33,9 +34,11 @@ class DataPipeline:
         self.NORMALIZED_DIR = os.path.join('data', 'normalized', self.DATASET)
 
     def create_bucket(self, df):
+        
         print("Creating the buckets...")
         buckets = {}
         G=nx.Graph()
+        
         for row in tqdm(df.iterrows()):
             bug_id = row[1]['issue_id']
             dup_id = row[1]['dup_id']
@@ -43,6 +46,7 @@ class DataPipeline:
                 G.add_node(bug_id)
             else:
                 G.add_edges_from([(int(bug_id), int(dup_id))])
+        
         for g in tqdm(nx.connected_components(G)):
             group = set(g)
             for bug in g:
@@ -52,6 +56,7 @@ class DataPipeline:
                     group.remove(master)
                     master = np.random.choice(list(group), 1)
             buckets[int(master)] = group
+        
         return buckets
 
     def save_buckets(self, buckets):
@@ -59,10 +64,13 @@ class DataPipeline:
             pickle.dump(buckets, f)
 
     def setup(self):
+        
         # create 'dataset' directory
         bug_dir = os.path.join(self.DIR_OUTPUT, self.DATASET)
+        
         if not os.path.exists(self.DIR_OUTPUT):
             os.mkdir(self.DIR_OUTPUT)
+        
         if not os.path.exists(bug_dir):
             os.mkdir(bug_dir)
 
@@ -78,8 +86,10 @@ class DataPipeline:
         self.DOMAIN = os.path.join(self.NORMALIZED_DIR, self.DOMAIN)
 
     def load_train(self):
+        
         logger.debug('Loading : {}'.format(self.DOMAIN))
         df_train = pd.read_csv('{}.csv'.format(self.DOMAIN))
+        
         if self.BASE != 'firefox':
             df_train.columns = ['issue_id','bug_severity','bug_status','component',
                                 'creation_ts','delta_ts','description','dup_id','priority',
@@ -91,8 +101,10 @@ class DataPipeline:
         return df_train
 
     def get_tokenizer(self):
+        
         if self.PREPROCESSING == 'bert':
             return TokenizerBert()
+        
         if self.PREPROCESSING == 'base':
             return TokenizerBase()
         else:
@@ -102,11 +114,14 @@ class DataPipeline:
         
         # Setup directories
         self.setup()
+        
         # Train
         df_train = self.load_train()
+        
         # Create buckets
         buckets = self.create_bucket(df_train)
         self.save_buckets(buckets)
+        
         # Create pairs
         bug_pairs, bug_ids = Util.read_pairs(self.DIR_OUTPUT, buckets, df_train)
         bugs_id_dataset = df_train['issue_id'].values
@@ -120,11 +135,15 @@ class DataPipeline:
 
         # Transformation config
         tokenizer = self.get_tokenizer()
+        
         # Create pipeline
         data_pipeline = DataTransformation(self, tokenizer)
+        
         # Step to select what fields to process
         text = data_pipeline.select_fields_stage(bug_ids, df_train)
+        
         # Step to build the vocab
         word_vocab = data_pipeline.build_vocabulary_stage(text, self.MAX_NB_WORDS)
+        
         # Step to preprocess the dump of bugs
         data_pipeline.save_bugs_stage(word_vocab, bug_ids, bugs_id_dataset)
